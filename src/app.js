c5992c4f78669b1f7dfd95686e8b2c1e8cd127be
zhaolu <1632089314@qq.com>
2016-07-25 22:20:17 +0800

    move buildReceiptItems function to ReceiptItem.buildReceiptItems method

--- src/app.js
+++ src/app.js
@@ -7,43 +7,11 @@ const Promotion = require('./models/promotion');
 function printReceipt(tags) {
 
   const cartItems = CartItem.buildCartItems(tags, Item.all());
-  const receiptItems = buildReceiptItems(cartItems, Promotion.all());
+  const receiptItems = ReceiptItem.buildReceiptItems(cartItems, Promotion.all());
   const receipt = buildReceipt(receiptItems);
   const receiptText = buildReceiptText(receipt);
 
   console.log(receiptText);
-}
-
-function buildReceiptItems(cartItems, allPromotions) {
-  return cartItems.map(cartItem => {
-
-    const promotionType = findPromotionType(cartItem.item.barcode, allPromotions);
-
-    const {saved, subtotal} = discount(cartItem.count, cartItem.item.price, promotionType);
-
-    return new ReceiptItem(cartItem, saved, subtotal);
-  });
-}
-
-function discount(count, price, promotionType) {
-
-  let subtotal = count * price;
-  let saved = 0;
-
-  if (promotionType === 'BUY_TWO_GET_ONE_FREE') {
-    saved = parseInt(count / 3) * price;
-  }
-
-  subtotal -= saved;
-
-  return {saved, subtotal};
-}
-
-function findPromotionType(barcode, promotions) {
-
-  const promotion = promotions.find(promotion => promotion.barcodes.some(b => b === barcode));
-
-  return promotion ? promotion.type : undefined;
 }
 
 function buildReceipt(receiptItems) {
--- src/models/receipt-item.js
+++ src/models/receipt-item.js
@@ -5,6 +5,39 @@ class ReceiptItem {
     this.saved = saved;
     this.subtotal = subtotal;
   }
+
+  static buildReceiptItems(cartItems, allPromotions) {
+
+    const discount = (count, price, promotionType) => {
+
+      let subtotal = count * price;
+      let saved = 0;
+
+      if (promotionType === 'BUY_TWO_GET_ONE_FREE') {
+        saved = parseInt(count / 3) * price;
+      }
+
+      subtotal -= saved;
+
+      return {saved, subtotal};
+    };
+
+    const findPromotionType = (barcode, promotions) => {
+
+      const promotion = promotions.find(promotion => promotion.barcodes.some(b => b === barcode));
+
+      return promotion ? promotion.type : undefined;
+    };
+
+    return cartItems.map(cartItem => {
+
+      const promotionType = findPromotionType(cartItem.item.barcode, allPromotions);
+
+      const {saved, subtotal} = discount(cartItem.count, cartItem.item.price, promotionType);
+
+      return new ReceiptItem(cartItem, saved, subtotal);
+    });
+  }
 }
 
 module.exports = ReceiptItem;
