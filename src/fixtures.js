012d60836b9f47f488f02970d8e4a18a0a4680dd
zhaolu <1632089314@qq.com>
2016-07-25 20:10:06 +0800

    replace item object by Item instance

--- src/fixtures.js
+++ src/fixtures.js
@@ -1,41 +1,13 @@
+const Item = require('./models/item');
+
 function loadAllItems() {
   return [
-    {
-      barcode: 'ITEM000000',
-      name: '可口可乐',
-      unit: '瓶',
-      price: 3.00
-    },
-    {
-      barcode: 'ITEM000001',
-      name: '雪碧',
-      unit: '瓶',
-      price: 3.00
-    },
-    {
-      barcode: 'ITEM000002',
-      name: '苹果',
-      unit: '斤',
-      price: 5.50
-    },
-    {
-      barcode: 'ITEM000003',
-      name: '荔枝',
-      unit: '斤',
-      price: 15.00
-    },
-    {
-      barcode: 'ITEM000004',
-      name: '电池',
-      unit: '个',
-      price: 2.00
-    },
-    {
-      barcode: 'ITEM000005',
-      name: '方便面',
-      unit: '袋',
-      price: 4.50
-    }
+    new Item('ITEM000000', '可口可乐', '瓶', 3.00),
+    new Item('ITEM000001', '雪碧', '瓶', 3.00),
+    new Item('ITEM000002', '苹果', '斤', 5.50),
+    new Item('ITEM000003', '荔枝', '斤', 15.00),
+    new Item('ITEM000004', '电池', '个', 2.00),
+    new Item('ITEM000005', '方便面', '袋', 4.50)
   ];
 }
 
