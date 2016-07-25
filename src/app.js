2e033756c2101e9de401645a7dc242cf7d0031a8
zhaolu <1632089314@qq.com>
2016-07-25 23:04:31 +0800

    move buildReceiptText to receipt.buildText method

--- src/app.js
+++ src/app.js
@@ -9,33 +9,9 @@ function printReceipt(tags) {
   const cartItems = CartItem.buildCartItems(tags, Item.all());
   const receiptItems = ReceiptItem.buildReceiptItems(cartItems, Promotion.all());
   const receipt = Receipt.buildReceipt(receiptItems);
-  const receiptText = buildReceiptText(receipt);
+  const receiptText = receipt.buildText();
 
   console.log(receiptText);
-}
-
-function buildReceiptText(receipt) {
-
-  let receiptItemsText = receipt.receiptItems
-    .map(receiptItem => {
-      const cartItem = receiptItem.cartItem;
-      return `名称：${cartItem.item.name}，\
-数量：${cartItem.count}${cartItem.item.unit}，\
-单价：${formatMoney(cartItem.item.price)}(元)，\
-小计：${formatMoney(receiptItem.subtotal)}(元)`;
-    })
-    .join('\n');
-
-  return `***<没钱赚商店>收据***
-${receiptItemsText}
-----------------------
-总计：${formatMoney(receipt.total)}(元)
-节省：${formatMoney(receipt.savedTotal)}(元)
-**********************`;
-}
-
-function formatMoney(money) {
-  return money.toFixed(2);
 }
 
 exports.printReceipt = printReceipt;
--- src/models/receipt.js
+++ src/models/receipt.js
@@ -1,9 +1,33 @@
 class Receipt {
 
-  constructor(receiptItems = [], total = 0, savedTotal = 0) {
+  constructor(receiptItems = [], savedTotal = 0, total = 0) {
     this.receiptItems = receiptItems;
-    this.total = total;
     this.savedTotal = savedTotal;
+    this.total = total;
+  }
+
+  buildText() {
+
+    const formatMoney = (money) => {
+      return money.toFixed(2);
+    };
+
+    let receiptItemsText = this.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
   }
 
   static buildReceipt(receiptItems) {
@@ -16,7 +40,7 @@ class Receipt {
       savedTotal += receiptItem.saved;
     }
 
-    return new Receipt(receiptItems, total, savedTotal);
+    return new Receipt(receiptItems, savedTotal, total);
   }
 }
 
