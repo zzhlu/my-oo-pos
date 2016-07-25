856a8dbe179d093a2df53783304d8f415d161387
zhaolu <1632089314@qq.com>
2016-07-25 21:57:23 +0800

    move buildCartItems function to CartItem.buildCartItems method

--- src/app.js
+++ src/app.js
@@ -6,35 +6,12 @@ const Promotion = require('./models/promotion');
 
 function printReceipt(tags) {
 
-  const cartItems = buildCartItems(tags, Item.all());
+  const cartItems = CartItem.buildCartItems(tags, Item.all());
   const receiptItems = buildReceiptItems(cartItems, Promotion.all());
   const receipt = buildReceipt(receiptItems);
   const receiptText = buildReceiptText(receipt);
 
   console.log(receiptText);
-}
-
-function buildCartItems(tags, allItems) {
-
-  const cartItems = [];
-
-  for (const tag of tags) {
-
-    const tagArray = tag.split('-');
-    const barcode = tagArray[0];
-    const count = parseFloat(tagArray[1] || 1);
-
-    const cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
-
-    if (cartItem) {
-      cartItem.count += count;
-    } else {
-      const item = allItems.find(item => item.barcode === barcode);
-      cartItems.push(new CartItem(item, count));
-    }
-  }
-
-  return cartItems;
 }
 
 function buildReceiptItems(cartItems, allPromotions) {
--- src/models/cart-item.js
+++ src/models/cart-item.js
@@ -4,6 +4,29 @@ class CartItem {
     this.item = item;
     this.count = count;
   }
+
+  static buildCartItems(tags, allItems) {
+
+    const cartItems = [];
+
+    for (const tag of tags) {
+
+      const tagArray = tag.split('-');
+      const barcode = tagArray[0];
+      const count = parseFloat(tagArray[1] || 1);
+
+      const cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+
+      if (cartItem) {
+        cartItem.count += count;
+      } else {
+        const item = allItems.find(item => item.barcode === barcode);
+        cartItems.push(new CartItem(item, count));
+      }
+    }
+
+    return cartItems;
+  }
 }
 
 module.exports = CartItem;
